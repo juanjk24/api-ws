4822406bc5327badcb469cb2d6529238edfce898
juanjk24 <juanjkuellar24@gmail.com>
2024-03-22 16:41:13 -0500

    add send  message with timeout

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,44 @@
-import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from "@bot-whatsapp/bot";
+import {
+  createBot,
+  createFlow,
+  MemoryDB,
+  createProvider,
+  addKeyword,
+} from "@bot-whatsapp/bot";
 import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys";
 
 //aqui se pueden agregar diversos mensajes automaticos
-const flowBienvenida = addKeyword('hola').addAnswer('¡Hola! Este es un mensaje Automatico')
+const flowBienvenida = addKeyword("hola").addAnswer(
+  "¡Hola! Este es un mensaje Automatico"
+);
 
-const main = async () =>{
+const main = async () => {
+  const provider = createProvider(BaileysProvider);
+  provider.initHttpServer(3002);
 
-    const provider = createProvider(BaileysProvider)
+  provider.http?.server.post(
+    "/send-message",
+    handleCtx(async (bot, req, res) => {
+      const phone = req.body.phone;
+      const message = req.body.message;
+      const time = req.body.time;
+      //const mediaUrl = req.body.mediaUrl
 
-    provider.initHttpServer(3002)
-
-    provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) =>{
-        
-        const phone = req.body.phone
-        const message = req.body.message
-        //const mediaUrl = req.body.mediaUrl
-
-
+      setTimeout(async () => {
         await bot.sendMessage(`57${phone}`, message, {
-            //media: mediaUrl
-        })
-        res.end('esto es del servidor')
-    }))
+          //media: mediaUrl
+        });
+      }, time);
 
-    await createBot({
-        flow: createFlow([flowBienvenida]),
-        database: new MemoryDB,
-        provider
+      res.end("esto es del servidor");
     })
+  );
 
-}
+  await createBot({
+    flow: createFlow([flowBienvenida]),
+    database: new MemoryDB(),
+    provider,
+  });
+};
 
-main()
+main();
