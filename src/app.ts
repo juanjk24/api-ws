70540bf1f27e35062f2e871c4ec4d03ac86fb3f1
juanjk24 <juanjkuellar24@gmail.com>
2024-03-22 20:03:59 -0500

    update send message with timestamp

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,16 @@ const flowBienvenida = addKeyword("hola").addAnswer(
 );
 
 const main = async () => {
+  // Definir la fecha de vencimiento
+  const fechaVencimiento = new Date("2024-03-22T22:00:00"); // 10pm del día 22/03/2024
+
+  // Calcular dos horas antes de la fecha de vencimiento
+  const dosHorasAntes = fechaVencimiento.getTime() - 2 * 60 * 60 * 1000; // 2 horas en milisegundos
+
+  //tiempo restante en Timestamp (milisegundos)
+  const tiempoRestante = dosHorasAntes - Date.now();
+
+  //iniciamos el servidor
   const provider = createProvider(BaileysProvider);
   provider.initHttpServer(3002);
 
@@ -21,16 +31,16 @@ const main = async () => {
     handleCtx(async (bot, req, res) => {
       const phone = req.body.phone;
       const message = req.body.message;
-      const time = req.body.time;
+      //const time = req.body.time;
       //const mediaUrl = req.body.mediaUrl
 
       setTimeout(async () => {
         await bot.sendMessage(`57${phone}`, message, {
           //media: mediaUrl
         });
-      }, time);
+      }, tiempoRestante);
 
-      res.end("esto es del servidor");
+      res.end("Mensaje enviado");
     })
   );
 
