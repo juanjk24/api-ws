db7bf6ff09985a453566ef78f43dd4de74da1a62
juanjk24 <juanjkuellar24@gmail.com>
2024-03-22 20:12:55 -0500

    add send message with timestamp

--- src/app.ts
+++ src/app.ts
@@ -13,19 +13,6 @@ const flowBienvenida = addKeyword("hola").addAnswer(
 );
 
 const main = async () => {
-  // Definir la fecha de vencimiento
-  const fechaVencimiento = new Date("2024-03-22T22:00:00"); // 10pm del día 22/03/2024
-
-  // Calcular dos horas antes de la fecha de vencimiento
-  const dosHorasAntes = fechaVencimiento.getTime() - 2 * 60 * 60 * 1000; // 2 horas en milisegundos
-
-<<<<<<< HEAD
-  //tiempo restante en Timestamp (milisegundos)
-=======
-  //tiempo restante en Timestamp 
->>>>>>> 17321ab1c23c76ba7a3d6742a31b038c4f7a6dff
-  const tiempoRestante = dosHorasAntes - Date.now();
-
   //iniciamos el servidor
   const provider = createProvider(BaileysProvider);
   provider.initHttpServer(3002);
@@ -35,8 +22,17 @@ const main = async () => {
     handleCtx(async (bot, req, res) => {
       const phone = req.body.phone;
       const message = req.body.message;
-      //const time = req.body.time;
+      const date = req.body.date;
       //const mediaUrl = req.body.mediaUrl
+
+      // Definir la fecha de vencimiento
+      const fechaVencimiento = new Date(date); // 10pm del día 22/03/2024
+
+      // Calcular dos horas antes de la fecha de vencimiento
+      const dosHorasAntes = fechaVencimiento.getTime() - 2 * 60 * 60 * 1000; // 2 horas en milisegundos
+
+      //tiempo restante en Timestamp (milisegundos)
+      const tiempoRestante = dosHorasAntes - Date.now();
 
       setTimeout(async () => {
         await bot.sendMessage(`57${phone}`, message, {
